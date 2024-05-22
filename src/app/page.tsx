4e5ffed577f5d26fc35831b2e5a10df04dfcdfdd
S-jooyoung <jooyoung.dev@gmail.com>
2024-05-22 16:36:34 +0000

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
-      <div className="font-pretendard font-semibold">위픽 서비스 프리텐다드</div>
+      <div className="font-pretendard font-semibold">
+        위픽 서비스 프리텐다드
+      </div>
       <div className="font-allroundgothic ">Wepik Service All Round Gothic</div>
       <Button>버튼</Button>
       <Input></Input>
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      
+      
     </>
   );
 }

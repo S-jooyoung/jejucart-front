3182bde2ce993da7c11072d0bcdca05a585d6dab
shin jooyoung <young@staybility.co.kr>
2024-05-23 01:29:08 +0900

    CI: 배포 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
-      <div className="font-pretendard font-semibold">
-        위픽 서비스 프리텐다드
-      </div>
+      <div className="font-pretendard font-semibold">위픽 서비스 프리텐다드</div>
       <div className="font-allroundgothic ">Wepik Service All Round Gothic</div>
       <Button>버튼</Button>
       <Input></Input>
@@ -36,7 +25,6 @@ export default function Home() {
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
-      <Calendar />
     </>
   );
 }
